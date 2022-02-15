0496627c614878043f5a2f491b90f84f882a27ad
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-02-15 11:21:22 +0200

    Update utils.js

--- backend/utils.js
+++ backend/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (user) => {
         isAdmin: user.isAdmin,
        
       },
-      process.env.JWT_SECRET || 'somethingsecret',
+      process.env.JWT_SECRET || 'simplekey',
       {
         expiresIn: '30d',
       }
@@ -20,7 +20,7 @@ export const isAuth = (req, res, next) =>{
     const authorization = req.headers.authorization;
     if(authorization){
         const token = authorization.slice(7, authorization.length);
-        jwt.verify(token, process.env.JWT_SECRET || 'somethingsecret', 
+        jwt.verify(token, process.env.JWT_SECRET || 'simplekey', 
         (err, decode) =>{
                 if(err){
                     res.status(401).send({medssage:"Invalid Token"});
