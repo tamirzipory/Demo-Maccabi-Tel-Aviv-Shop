a2206032ca109f67b15a4ac19b539851c2d36774
tamir zipory <58652947+tamirzipory@users.noreply.github.com>
2022-02-15 11:20:05 +0200

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,3 @@
-export const MONGODB_URL = "mongodb+srv://tamirzip:tamir0202@tamirpro.u3qlj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const MONGODB_URL = "mongodb+srv://tamirzip:<put_your_mongo_password>@tamirpro.u3qlj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 
